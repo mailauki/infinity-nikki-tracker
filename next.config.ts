f1bf3c18b7727a2e5dc8427f2cd6f20e3e65e23f
Julie Evans <juliemevans17@gmail.com>
2026-01-21 15:35:46 -0600

    Add theme colors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  turbopack: {
+    root: path.join(__dirname, '..'), // Sets the root to the parent directory
+  },
+  images: {
+		remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.wikia.nocookie.net',
+        port: '',
+        pathname: '/infinity-nikki/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
